9b8967f99d6b030a73b8460044da1743bc2982c3
sekishin <s14t242@stmail.eng.kagawa-u.ac.jp>
2015-11-06 18:38:32 +0900

    Update xp_game.rb

--- xp_game.rb
+++ xp_game.rb
@@ -5,12 +5,24 @@ TAX = {
   '4' => 4.00,
   '5' => 8.25
 }
-
-puts ''
+GOODS = {
+  '1' => 
+}
+puts '商品金額は？'
 price = gets.to_i
-puts ''
+puts '個数は？'
 num = gets.to_i
-puts ''
+puts '地域は？'
 place = gets.chomp
+sum = price * num * TAX[place]
+puts "#{sum.floor}"
 
-puts "sum: #{(price * num * TAX[place]).floor}"
+if sum >= 1000 && sum < 5000 
+  sum *= 0.97
+else if sum >= 5000 && sum < 7000
+  sum *= 0.95
+else if sum >= 7000 && sum < 10000
+  sum *= 0.9
+else if sum >= 10000 && xum < 500000
+  sum *= 0.85
+end
